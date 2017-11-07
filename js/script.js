83135272e7b4eca508c8ed6d663b44b090a41935
radchenko viacheslav <mibdeveloper@gmail.com>
2017-11-07 16:51:56 +0200

    handler for modal window is done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 $(function(){
+	
+
+	$("#exampleInputEmail1").val("aasdf@asdf.ty");
+	$("#exampleInputPassword1").val("1234567");
+	$("#exampleInputPassword2").val("1234567");
+
 	$("#signupSubmit").click(function(){
-		alert("dfgsdf");
+		let $form = $(this).closest(".modal-content")
+		let email = $form.find("#exampleInputEmail1").val();
+		let password = $form.find("#exampleInputPassword1").val();
+		let passwordConfirmation = $form.find("#exampleInputPassword2").val();
+		
+		if((password.length  > 3) && (password == passwordConfirmation)){
+			//TODO: Send message
+		}else{
+			//TODO: Error message
+			alert("Password Error");
+		}
 	});
 })
